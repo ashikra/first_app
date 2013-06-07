1be57965101f25f16fb3d537fd3537ada2864670
U-Brain-PC\Brain <Brain@Brain-PC.(none)>
2013-06-07 11:42:29 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '320eee9d4d0cd6a60e30a6586dc74cd64618b4ed0badae9d18af325dcd5115335a2a92cbb7db3aa9cd75eecaf617db6cad4414c2983a61c2f61a2ac9d42de9be'
